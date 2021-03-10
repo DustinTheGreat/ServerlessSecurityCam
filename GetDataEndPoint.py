af136ce5786e9b31faf0e0daccc614703a88095f
dustinthegreat <dustinroberts96@gmail.com>
2021-03-10 18:31:57 -0500

    wtf am i even using git

--- GetDataEndPoint.py
+++ GetDataEndPoint.py
@@ -1,4 +1,6 @@
 import boto3
+import webbrowser
+
 STREAM_NAME = "PIStream"
 kvs = boto3.client("kinesisvideo")
 # Grab the endpoint from GetDataEndpoint
@@ -6,34 +8,44 @@ endpoint = kvs.get_data_endpoint(
     APIName="GET_HLS_STREAMING_SESSION_URL",
     StreamName=STREAM_NAME
 )['DataEndpoint']
-# Grab the HLS Stream URL from the endpoint
-kvam = boto3.client("kinesis-video-archived-media", endpoint_url=endpoint)
-url = kvam.get_hls_streaming_session_url(
-    StreamName=STREAM_NAME,
-    PlaybackMode="LIVE",
-    Expires=300
-)['HLSStreamingSessionURL']
 
-print(url)
-# import HTML
-# HTML(data='<video src="{0}" autoplay="autoplay" controls="controls" width="300" height="400"></video>'.format(url)) 
+
+def main():
+	print("Grabbing Session Steaming Session")
+	print("----------------------------------")
+	url = get_streaming_session()
+	print("Opening Stream")
+	print("----------------------------------")
+	open_media(url)
 
 
 
+	# Grab the HLS Stream URL from the endpoint
+def get_streaming_session():
+	kvam = boto3.client("kinesis-video-archived-media", endpoint_url=endpoint)
+	url = kvam.get_hls_streaming_session_url(
+	    StreamName=STREAM_NAME,
+	    PlaybackMode="ON_DEMAND",
+	    Expires=300
+	)['HLSStreamingSessionURL']
 
-import webbrowser
+	return(url)
 
-f = open('index.html','w')
+def open_media(url):
+	f = open('index.html','w')
 
-message = """<html>
-<head></head>
-<body><p>Hello World!</p></br><video src='%s' autoplay="autoplay" controls="controls" width="300" height="400"></video></body>
-</html>"""
-x = message % (url)
-print(x)
-f.write(x)
-f.close()
+	message = """<html>
+	<head></head>
+	<body><p>Hello World!</p></br><video src='%s' autoplay="autoplay" controls="controls" width="300" height="400"></video></body>
+	</html>"""
+	x = message % (url)
+	print(x)
+	f.write(x)
+	f.close()
 
-#Change path to reflect file location
-filename = '/home/costanza/Tools/' + 'index.html'
-webbrowser.open_new_tab(filename)
+	#TODO: make user path for pi systems
+	filename = '/home/costanza/Tools/' + 'index.html'
+	webbrowser.open_new_tab(filename)
+
+if __name__ == '__main__':
+	main()
