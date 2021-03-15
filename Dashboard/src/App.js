6c2619e2bbf99c46b2be3cde694183d3e2711d17
Dustin <dustinroberts96@gmail.com>
2021-03-15 16:55:25 -0400

    Adding Auth w/Cognito

---
+++ Dashboard/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Signup from './Signup'
+import Signin from './Signin'
+import UserStatus from './UserStatus'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Signup />
+        <hr />
+        <Signin />
+        <UserStatus />
+      </div>
+    );
+  }
+}
+
+export default App;
