eaf6907eff048b246a13ab9370a3f7dded88da24
Dustin <dustinroberts96@gmail.com>
2021-03-27 17:08:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,17 @@
 apt-get update
 apt-get -y upgrade
 apt-get dist-upgrade
+cd /amazon-kinesis-video-streams-producer-sdk-cpp
+modprobe bcm2835-v4l2
+export GST_PLUGIN_PATH=`pwd`/build
+export LD_LIBRARY_PATH=`pwd`/open-source/local/lib
+gst-launch-1.0 v4l2src device=/dev/video0 \
+! videoconvert \
+! video/x-raw,format=I420,width=640,height=480 \
+! omxh264enc control-rate=2 target-bitrate=512000 periodicity-idr=45 inline-header=FALSE \
+! h264parse ! video/x-h264,stream-format=avc,alignment=au,profile=baseline \
+! kvssink stream-name="mytoasteroven" \
+access-key="AKIA56YUOEMN35BLJ75Q" \
+secret-key="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" \
+aws-region="us-east-1"
+
