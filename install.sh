d6c3719a2c67d3a6eeb9ded99d8f270b3bbc6eca
Dustin <dustinroberts96@gmail.com>
2021-03-28 00:20:56 -0400

    whoops

--- install.sh
+++ install.sh
@@ -6,16 +6,6 @@ cd /amazon-kinesis-video-streams-producer-sdk-cpp
 modprobe bcm2835-v4l2
 export GST_PLUGIN_PATH=`pwd`/build
 export LD_LIBRARY_PATH=`pwd`/open-source/local/lib
-gst-launch-1.0 v4l2src device=/dev/video0 \
-! videoconvert \
-! video/x-raw,format=I420,width=640,height=480 \
-! omxh264enc control-rate=2 target-bitrate=512000 periodicity-idr=45 inline-header=FALSE \
-! h264parse ! video/x-h264,stream-format=avc,alignment=au,profile=baseline \
-! kvssink stream-name="mytoasteroven" \
-access-key="" \
-secret-key="" \
-aws-region="us-east-1"
-
 gst-launch-1.0 v4l2src do-timestamp=TRUE device=/dev/video0 \
   ! videoconvert
   ! video/x-raw,format=I420,width=640,height=480,framerate=30/1
@@ -23,6 +13,6 @@ gst-launch-1.0 v4l2src do-timestamp=TRUE device=/dev/video0 \
   ! h264parse 
   ! video/x-h264,stream-format=avc,alignment=au,width=640,height=480,framerate=30/1,profile=baseline
   ! kvssink stream-name="PiStream3"
-     access-key="AKIA56YUOEMNQKHZR4NJ"
-     secret-key="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"
+     access-key=""
+     secret-key=""
      aws-region="us-east-1"
