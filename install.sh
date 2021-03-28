d1c1dd82a2f98724e03bbe8035dd007d21dd6c6b
Dustin <dustinroberts96@gmail.com>
2021-03-28 00:18:07 -0400

    updating install script

--- install.sh
+++ install.sh
@@ -16,3 +16,13 @@ access-key="" \
 secret-key="" \
 aws-region="us-east-1"
 
+gst-launch-1.0 v4l2src do-timestamp=TRUE device=/dev/video0 \
+  ! videoconvert
+  ! video/x-raw,format=I420,width=640,height=480,framerate=30/1
+  ! omxh264enc control-rate=1 target-bitrate=51200 periodicty-idr=45 inline-header=FALSE
+  ! h264parse 
+  ! video/x-h264,stream-format=avc,alignment=au,width=640,height=480,framerate=30/1,profile=baseline
+  ! kvssink stream-name="PiStream3"
+     access-key="AKIA56YUOEMNQKHZR4NJ"
+     secret-key="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"
+     aws-region="us-east-1"
